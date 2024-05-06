8a1f26d1b473f9ce3a2a9584cf50e6ba044ddb08
rafal-gorecki <rafal.gorecki@husarion.com>
2024-05-06 16:33:11 +0200

    Pawel suggestions

--- README.md
+++ README.md
@@ -1,22 +1,46 @@
-# Husarion office world 
+# Husarion Gazebo Worlds
 
-Custom made gazebo worlds.
+A repository containing sample worlds to run in the gazebo simulation.
 
-## How to run
+**Available worlds:**
 
-Prepare workspace and run Gazebo:
+- `empty_with_plugins.sdf` - Empty world with attached plugins necessary for the proper operation of our robots,
+- `husarion_office.sdf` - World resembling the office we work in,
+- `husarion_world.sdf` - World representing our logo.
+
+## Quick start
+
+### Create workspace
 
 ```bash
 mkdir -p ~/ros2_ws
 cd ~/ros2_ws
-git clone https://github.com/husarion/husarion_office_gz.git src/husarion_office_gz
-
-colcon build
-source install/setup.bash
-ros2 launch husarion_office_gz gz_sim.launch.py
+git clone https://github.com/husarion/husarion_gz_worlds.git src/husarion_gz_worlds
 ```
 
-## Results
+### Build
+
+```bash
+colcon build
+```
+
+### Running
+
+```bash
+source install/setup.bash
+ros2 launch husarion_gz_worlds gz_sim.launch.py
+```
+
+**Launch arguments:**
+
+| ARGUMENTS         | DESCRIPTION                                                                                                  | VALUE                                                      |
+| ----------------- | ------------------------------------------------------------------------------------------------------------ | ---------------------------------------------------------- |
+| **gz_log_level**  | Adjust the verbosity of the console output                                                                   | **1**                                                      |
+| **headless_mode** | Run the simulation in headless mode. Useful when a GUI is not needed or to reduce the amount of calculations | **False**                                                  |
+| **world**         | Absolute path to SDF world file                                                                              | **`husarion_gz_worlds/`<br />`worlds/husarion_world.sdf`** |
+
+### Results
+
 ![img1](./images/office1.png)
 ![img2](./images/office2.png)
 ![img3](./images/husarion_world.png)
--- launch/gz_sim.launch.py
+++ launch/gz_sim.launch.py
@@ -1,8 +1,31 @@
+#!/usr/bin/env python3
+
+# Copyright 2024 Husarion sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from launch_ros.substitutions import FindPackageShare
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+
 
 def launch_setup(context, *args, **kwargs):
     gz_log_level = LaunchConfiguration("gz_log_level").perform(context)
@@ -30,18 +53,26 @@ def launch_setup(context, *args, **kwargs):
 
 
 def generate_launch_description():
-    pkg_path = FindPackageShare("husarion_office_gz")
+    pkg_path = FindPackageShare("husarion_gz_worlds")
 
     declare_gz_log_level = DeclareLaunchArgument(
-        "gz_log_level", default_value="1", description="Adjust the level of console output.", choices=["0", "1", "2", "3", "4"]
+        "gz_log_level",
+        default_value="1",
+        description="Adjust the level of console output.",
+        choices=["0", "1", "2", "3", "4"],
     )
 
     declare_headless_mode = DeclareLaunchArgument(
-        "headless_mode", default_value="False", description="Run the simulation in headless mode. Useful when a GUI is not needed or to reduce the amount of calculations.", choices=["True", "False"]
+        "headless_mode",
+        default_value="False",
+        description="Run the simulation in headless mode. Useful when a GUI is not needed or to reduce the amount of calculations.",
+        choices=["True", "False"],
     )
 
     declare_world_arg = DeclareLaunchArgument(
-        "world", default_value=PathJoinSubstitution([pkg_path, "worlds", "husarion_world.sdf"]), description="Path to SDF world file."
+        "world",
+        default_value=PathJoinSubstitution([pkg_path, "worlds", "husarion_world.sdf"]),
+        description="Absolute path to SDF world file.",
     )
 
     return LaunchDescription(
